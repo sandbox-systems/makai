43444b78fa4513fc371069244b613c80b162e072
Shriggs <spm1215@gmail.com>
2019-01-11 22:47:57 -0600

    Add firebase credentials on backend for global use

---
+++ firebase/credentials.py
@@ -0,0 +1,9 @@
+# Set up firebase web credentials for global use
+config = {
+    "apiKey": "AIzaSyBQgPQC1Zzky-8zJbFAO_8bkA29Ycq_kG4",
+    "authDomain": "sandbox-9291d.firebaseapp.com",
+    "databaseURL": "https://sandbox-9291d.firebaseio.com",
+    "projectId": "sandbox-9291d",
+    "storageBucket": "sandbox-9291d.appspot.com",
+    "messagingSenderId": "177612955416"
+}
