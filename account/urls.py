edcc0107367a43693bfa423945bf43f20fba9c47
Shivashriganesh Mahato <spm1215@gmail.com>
2019-01-30 07:53:15 -0600

    Handle account syncing callback and ensure code was returned
    TODO: fetch token from code and proceed from there

--- account/urls.py
+++ account/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'login', views.login, name="Login"),
-    url(r'callback', views.callback, name="Callback"),
+    url(r'login$', views.login, name="Login"),
+    url(r'login/callback', views.login_callback, name="LoginCallback"),
     url(r'username/$', views.settings, name="Settings"),
-    url(r'username/sync', views.sync, name="Sync")
+    url(r'username/sync$', views.sync, name="Sync"),
+    url(r'username/sync/callback', views.sync_callback, name="SyncCallback")
 ]
--- account/views.py
+++ account/views.py
@@ -10,7 +10,7 @@ def login(request):
     return render(request, 'account/login.html', {'config': config})
 
 
-def callback(request):
+def login_callback(request):
     request.session['uid'] = request.POST.get('uid')
     return redirect('home:Home')
 
@@ -23,3 +23,16 @@ def sync(request):
     init_tokens(request)
     init_vcs()
     return render(request, 'account/sync.html', {'accounts': accounts.items()})
+
+
+def sync_callback(request):
+    # Ensure request is GET and contains code
+    if request.method == 'GET' and 'code' in request.GET:
+        code = request.GET['code']
+
+        # Ensure code has a value
+        if code:
+            return render(request, 'account/syncCallback.html')
+    # If there was an error anywhere in the process
+    return render(request, 'account/syncCallbackErrored.html')
+
