c5ec232ac38ba81ad9a6ea1dfcc12382d250c93a
Aadhithya Kannan <aadhi0319@gmail.com>
2019-03-20 15:38:54 -0500

    Committing script.js before switching branches

--- sandbox/static/sandbox/js/script.js
+++ sandbox/static/sandbox/js/script.js
@@ -143,6 +143,19 @@ $("#treeview").on("click", function (event) {
     event.preventDefault();
 });
 
+$("#treeview").on("contextmenu", ".node-treeview", function(key, options) {
+        var m = "clicked: " + key;
+        window.console && console.log(m) || alert(m);
+        return false;
+    },
+    {
+        "edit": {name: "Edit", icon: "edit"},
+        "cut": {name: "Cut", icon: "cut"},
+        "copy": {name: "Copy", icon: "copy"},
+        "paste": {name: "Paste", icon: "paste"},
+        "delete": {name: "Delete", icon: "delete"},
+        "sep1": "---------"
+    });
 
 //Breadcrumbs
 function setBreadcrumb(data) {
