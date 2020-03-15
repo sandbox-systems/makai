85b2f44bc19f03b3ee55d2ea66ea5dcb24fd1f6a
Shivashriganesh Mahato <spm1215@gmail.com>
2020-03-15 17:18:36 -0500

    Update credentials.py

--- vcs/credentials.py
+++ vcs/credentials.py
@@ -1,14 +1,16 @@
+# COPY AND PASTE FROM RESPECTIVE VCS HOST
+
 # Define all hosting services available for use
 vcs_hosts = ['github', 'bitbucket']
 
 # Set up github app credentials for global use
 github_config = {
-    'client_id': '2be96338b2d8b6b9d8d6',
-    'client_secret': '423cdd817e03b6241922d4396f879194aabbe998'
+    'client_id': '<>',
+    'client_secret': '<>'
 }
 
 # Set up bitbucket app credentials for global use
 bitbucket_config = {
-    'client_id': 'QjQ49apJFL26jg8gzY',
-    'client_secret': '9QgdTN6pgZd9hNxStr6HeswgweZ2RpjT'
+    'client_id': '<>',
+    'client_secret': '<>'
 }
